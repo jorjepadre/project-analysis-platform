b418e2d99d4d928009bee4b231b5ae495c972ba9
Jack Ong <jqong@connect.ust.hk>
2024-02-28 22:45:17 +0800

    feat: debank initializer

--- src/adapters/debank.ts
+++ src/adapters/debank.ts
@@ -1,11 +1,31 @@
+import { Browser, chromium } from 'playwright';
+
+class DeBankAdapter {
+  private static adapter: DeBankAdapter | null = null;
+
+  constructor(private browser: Browser) {}
+
+  static async init() {
+    if (this.adapter !== null) {
+      return this.adapter;
+    }
+
+    const browser = await chromium.launch();
+    const adapter = new DeBankAdapter(browser);
+    DeBankAdapter.adapter = adapter;
+    return adapter;
+  }
+
+  async fetchPools() {
+    const page = await this.browser.newPage();
+    const res = await page.goto('https://api.debank.com/protocol/pools?start=0&limit=20&id=curve&name=');
+    console.log(await res?.json());
+  }
+}
+
 async function main() {
-  console.log('main');
+  const adapter = await DeBankAdapter.init();
+  await adapter.fetchPools();
 }
 
 main();
-
-class DeBankAdapter {
-  async fetchProjects() {
-    return null;
-  }
-}
